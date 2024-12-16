89c93e812e1651ba2c0667a3babcef0508a0b096
Marcos Vinicius <marcos_archilla96@outlook.com>
2024-12-16 19:08:17 -0300

    first commit

---
+++ Desafio_Dio_Ruby/teste.rb
@@ -0,0 +1,10 @@
+#Projeto prático : Criando seu primeiro projeto com rubi
+
+print " Escreva seu nome completo: "
+nome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+
+print " Seu nome é #{nome}, e você tem #{idade}"
+
